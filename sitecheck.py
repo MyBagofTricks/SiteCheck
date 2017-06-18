cda48a499b4d5afc4758c29220dbef5a4bd0815b
MyBagofTricks <ipulldarters@gmail.com>
2017-06-17 18:30:01 -0700

    misc tweaks

--- sitecheck.py
+++ sitecheck.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Checks the status of ips
+# Checks the status of ips by connecting to a port and reporting on the result
 
 import socket
 import smtplib
@@ -27,7 +27,7 @@ def loadconf(f_name):
         raise SystemExit
 
 
-def portcheck(rhost, rport):
+def portcheck(host, port):
     """Uses socket to test a specified port
     rhost (str) - ip or hostname of target
     rport (int) = port to test
@@ -35,67 +35,96 @@ def portcheck(rhost, rport):
     """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(10)
-    result = s.connect_ex((rhost, rport))
+    result = s.connect_ex((host, port))
     s.close()
     if result:
         return False
     return True
 
 
-def callforhelp(site_down, creds, rport):
+def callforhelp(msg, creds):
     """Sends an email via google api
-    site_down(dict) - sites to email about. format {'site':'ip'}
+    msg (str) - text for email message
+    creds(dict) = email info keys: from, to, subject
+    Returns the msgid if success, error if failed
     """
-    msg_text = "Cannot connect to site(s) on port {port} for 10mins+".format(port=rport)
-    for site in site_down.keys():
-        msg_text+= "\n{site}\t{ip}".format(site=site, ip=site_down[site])
-    msg_text += "\n\nInvestigate and contact support\n\nI AM A BOT - DO NOT REPLY"
-    msgid = emailer.compose_and_send(
-        creds['from'], creds['to'], creds['subject'], msg_text
+    return emailer.compose_and_send(
+        creds['from'], creds['to'], creds['subject'], msg
         )
-    return msgid
+
+def messagebuilder(name, ip, port, dtime):
+    """builds and returns email text
+    name(str) - site name
+    ip(str) - ip
+    port(int) - port used
+    dtime(str) - date and time when site was last reachable
+    """
+    msg_text = """WARNING! {name} IS OFFLINE!
+    Last online: {dtime}
+    Cannot connect to {ip} via port {port}
+
+    Please contact the {name} site and possibly the ISP
+
+    I AM A BOT - DO NOT REPLY
+    """.format(name=name, dtime=dtime, ip=ip, port=port)
+    return msg_text
 
 
-def downreminder(down_time):
-    # returns True if 4 hours have passed since down_time
-    if time.time() - down_time < 14400:
+def downreminder(dtime):
+    """returns True if 4 hours have passed since down_time
+    dtime (float) - epoc time
+    """
+    if time.time() - dtime < 14400:
         return False
     else:
         return True
 
 
 def engine():
-    pass
-
-
-def main():
     settings = loadconf('sitecheck.config.json')
+    sites = settings['DEST']
+    port = settings['CONF']['port']
+    creds = settings['EMAIL']
+
     while True:
-        site_down = {}    # {store:{ip:xx,lastoline:xx}}
-        for site in settings['DEST'].keys():
-            if portcheck(settings['DEST'][site], settings['CONF']['port']) == False and portcheck('8.8.8.8',53) == True:
+        site_down = {}
+        for site, ip in sites.items():
+            if portcheck(ip, port) == False and portcheck('8.8.8.8',53) == True:
                 site_down[site] = {
-                    'IP': settings['DEST'][site],'lastonline': time.time(),
-                    'lastemailed': 0,
+                    'ip': ip,
+                    'dtime': time.time(),
+                    'lastemail': 0,
                     }
+                    # last func may be useful for fixing dict reset
         if site_down:
+            # double check list x10, pop off any that have come online, but log
             for x in range(1,11):
-                for site in site_down.keys():
-                    if portcheck(settings['DEST'][site], settings['CONF']['port']) == True:
+                for site, value in site_down.items():
+                    if portcheck(value['ip'], port) == True:
                         site_down.pop(site)
                         # log here
                 print('[!] {} site(s) unreachable. Attempt {} of 10 {}'.format(
                     len(site_down), x, time.ctime()))
-                time.sleep(10)
+                time.sleep(1)    # sleep bottleneck
             if portcheck('8.8.8.8', 53) == True:
                 for site, value in site_down.items():
-                    if site_down[site]['lastemailed'] == 0 or downreminder(site_down[site]['lastemailed']) == True:
-                        msgid = callforhelp(site_down, settings['EMAIL'], settings['CONF']['port'])
-                        print("[!] Email sent! ID: {msgid} {ctime} {site} down since {lastonline}".format(
-                            msgid=msgid, ctime=time.ctime(), site=site, lastonline=site_down[site]['lastonline']
-                        ))
+                    if downreminder(value['lastemail']) == True:
+                        normaltime = time.ctime(value['dtime'])
+                        msg = messagebuilder(
+                            site, value['ip'], port, normaltime
+                        )
+                        msgid = callforhelp(msg, settings['EMAIL'])
+                        print(
+                            "[!] Email sent! ID: {msgid} :: {site} down since {time}".format(
+                                msgid=msgid, ctime=time.ctime(), site=site, time=normaltime
+                                ))
+
         print("[ ] Sleeping for 30 min", time.ctime())
         time.sleep(1800)
-            
+
+
+def main():
+    engine()
+
 if __name__ == '__main__':
     main()
