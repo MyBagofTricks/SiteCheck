952ffeaa0b1d39fae9eb4d58845eec255157b452
MyBagofTricks <ipulldarters@gmail.com>
2017-06-20 14:05:16 -0700

    multiprocessing implemented

--- sitecheck.py
+++ sitecheck.py
@@ -7,6 +7,8 @@ import time
 import json
 from os import path
 
+import multiprocessing
+
 import emailer
 
 
@@ -20,121 +22,129 @@ def loadconf(f_name):
                 data = json.load(json_file)
                 return data
             except ValueError as err:
-                print("[x] Cannot continue: {err}".format(err=err))
+                message('x', "[x] Cannot continue: {err}".format(err=err))
                 raise SystemExit
     else:
-        print("[x] {f_name} not found. Exiting".format(f_name=f_name))
+        message('x', "[x] {f_name} not found. Exiting".format(f_name=f_name))
         raise SystemExit
 
 
-def portdown(host, port):
-    """Uses socket to test a specified port
-    host (str) - ip or hostname of target
-    port (int) = port to test
-    return False if online, time.time() if down
+def message(status, text):
+    """ Formats messages and prints to console
+    status(str) - error type (x means bad)
+    text(str) - text to print
+    """
+    print("[{status}] {time} :: {text}".format(
+        status=status, time=time.ctime(), text=text))
+
+
+def portdown(ip, port):
+    """ Checks if site is down
+    ip(str) - ip address
+    port(int) - port number
+    Returns true if down, false if up
     """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(10)
-    result = s.connect_ex((host, port))
+    result = s.connect_ex((ip, port))
     s.close()
-    if result:
-        return time.time()
-    return False
+    if not result:
+        return False
+    else:
+        return True
 
 
-def callforhelp(msg, creds):
-    """Sends an email via google api
-    msg (str) - text for email message
-    creds(dict) = email info keys: from, to, subject
-    Returns the msgid if success, error if failed
+def isdown(ip, port, retry):
+    """ Attempts to connect to an ip with retries if the ip/port are
+    not responding
+        ip(str) - ip address of target
+    port(int) - port address to use
+    retry(int) - number of times to retry
+    Returns time.time() if offline, True if online
     """
-    return emailer.compose_and_send(
-        creds['from'], creds['to'], creds['subject'], msg
-        )
+    for x in range(1, retry+1):
+        if not portdown(ip, port):
+            return ip, False
+        else:
+            message('x', "Site {} Down. Attempt {} of {} ".format(ip, x, retry))
+    return ip, time.time()
+        
 
-def messagebuilder(name, ip, port, dtime):
-    """builds and returns email text
-    name(str) - site name
-    ip(str) - ip
-    port(int) - port used
-    dtime(str) - date and time when site was last reachable
+def callforhelp(site, ip, port, dtime, creds):
+    """Creates and sends an email
+    site(str) - site name
+    ip(str) - site ip
+    port(int) - site port
+    dtime(str) - time and date
+    creds(dict) - contains to, from, subject fields
     """
-    msg_text = """WARNING! {name} IS OFFLINE!
+    msg = """WARNING! {name} IS OFFLINE!
     Last online: {dtime}
     Cannot connect to {ip} via port {port}
 
     Please contact the {name} site and possibly the ISP
 
     I AM A BOT - DO NOT REPLY
-    """.format(name=name, dtime=dtime, ip=ip, port=port)
-    return msg_text
+    """.format(name=site, dtime=dtime, ip=ip, port=port)
+    return emailer.compose_and_send(
+        creds['from'], creds['to'], creds['subject'], msg
+        )
 
 
-def isdown(ip, port, retry):
-    """Returns tuple epoc time if true, false if not (ip, time)
-    ip(str) - ip address of target
-    port(int) - port address to use
-    retry(int) - number of times to retry
-    """
-    for x in range(1, retry+1):
-        if not portdown(ip, port):
-            return (ip, False)
-        print("[x] {time} Error connecting to {ip} {x} out of {retry}".format(
-            time=time.ctime(), ip=ip, x=x, retry=retry))
-    return (ip, time.time())
+def engine(sites, port, creds, retry):
+    while True:
+        if portdown('8.8.8.8', 53) == False:
+            with multiprocessing.Pool(processes=3) as pool:
+                result = pool.starmap(
+                    isdown, {(ip, port, retry) for ip in sites.keys()}, 
+                    )
+            for tup in result:
+                ip = tup[0]
+                dtime = tup[1]
+                if not dtime:
+                    sites[ip]['dtime'] == False
+                else:
+                    if sites[ip]['dtime'] == False:
+                        sites[ip]['dtime'] = dtime
+            for ip, val in sites.items():
+                if val['dtime'] and time.time() - val['emailed'] > 14400:
+                    callforhelp(val['name'], ip, port, time.ctime(val['dtime']), creds)
+                    val['emailed'] = time.time()
+                    message('!', "Email sent! :: {site}".format(site=val['name']))
+                elif val['dtime'] > 0 and time.time() - val['emailed'] < 14400:
+                    message(
+                        '-', 'Skipped {site} - It was emailed {time} min ago'.format(
+                            site=val['name'], time=(time.time() - val['emailed'])//60)
+                            )
+            message(' ', "{time} Sleeping for 15mins".format(time=time.ctime()))
+            time.sleep(900)
+        else:
+            message('x', "Can't reach Google. Sleeping for an hour")
+            time.sleep(3600)
 
-        
-def engine():
+def generatedict(sites_ips):
+    # removed dtime check... keep simple
+    sites = {}
+    for site, ip in sites_ips.items():
+        sites[ip] = {
+            'name': site,
+            'dtime': False,
+            'emailed': False,
+        }
+    return sites
+
+
+def main():
+    pass
+    
+
+if __name__ == '__main__':
     settings = loadconf('sitecheck.config.json')
     port = settings['CONF']['port']
     creds = settings['EMAIL']
-    retry = 2
-    sites = {}
-    for site, ip in settings['DEST'].items():
-        sites[ip] = {
-            'name': site,
-            'dtime': portdown(ip, port),
-            'emailed': False,
-            }
+    retry = 5
+    sites = generatedict(settings['DEST'])
 
-    while True:
-        if portdown('8.8.8.8', 53) == False:
+    engine(sites, port, creds, retry)
 
-            for ip, val in sites.items():
-                down = isdown(ip, port, retry)
-                if down[1]:
-                    sites[ip]['dtime'] = down[1]
-                else:
-                    sites[ip]['dtime'] = False
-
-
-            # Send message
-            for ip, val in sites.items():
-                if val['dtime'] and time.time() - val['emailed'] > 14400:
-                    normaltime = time.ctime(val['dtime'])
-                    msg = messagebuilder(val['name'], ip, port, normaltime)
-                    msgid = callforhelp(msg, creds)
-                    val['emailed'] = time.time()
-                    print(
-                        "[!] Email sent! ID: {msgid} :: {site} down since {time}".format(
-                            msgid=msgid, ctime=time.ctime(), site=val['name'], time=normaltime)
-                            )
-                elif val['dtime'] > 0 and time.time() - val['emailed'] < 14400:
-                    print('skipped site because it was emailed {} min ago'.format(
-                        (time.time() - val['emailed'])//60))
-
-            print("[ ] {time} Sleeping for 30mins".format(time=time.ctime()))
-            # time.sleep(1800)
-            time.sleep(1)
-        else:
-            print("[x] {time} Can't reach Google. Sleeping for an hour".format(
-                time=time.ctime()
-            ))
-            time.sleep(3600)
-        
-
-def main():
-    engine()
-
-if __name__ == '__main__':
-    main()
+    
