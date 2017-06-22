7d5d8f59fbd8d3815bc7fcae1c20bb403aef66c5
MyBagofTricks <ipulldarters@gmail.com>
2017-06-21 17:54:32 -0700

    simplified code

--- sitecheck.py
+++ sitecheck.py
@@ -2,11 +2,9 @@
 # Checks the status of ips by connecting to a port and reporting on the result
 
 import socket
-import smtplib
 import time
 import json
-from os import path
-
+import os
 import multiprocessing
 
 import emailer
@@ -16,21 +14,21 @@ def loadconf(f_name):
     """ Checks for a json file, returns data if it exists, or else exits
     f_name(str) - name of the file
     """
-    if path.isfile(f_name):
+    if os.path.isfile(f_name):
         with open(f_name, 'r') as json_file:
             try:
                 data = json.load(json_file)
                 return data
             except ValueError as err:
-                message('x', "[x] Cannot continue: {err}".format(err=err))
+                alert('x', "Cannot continue: {err}".format(err=err))
                 raise SystemExit
     else:
-        message('x', "[x] {f_name} not found. Exiting".format(f_name=f_name))
+        alert('x', "{f_name} not found. Check the readme. Exiting".format(f_name=f_name))
         raise SystemExit
 
 
-def message(status, text):
-    """ Formats messages and prints to console
+def alert(status, text):
+    """ Formats alerts and prints to console
     status(str) - error type (x means bad)
     text(str) - text to print
     """
@@ -44,17 +42,13 @@ def portdown(ip, port):
     port(int) - port number
     Returns true if down, false if up
     """
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(10)
-    result = s.connect_ex((ip, port))
-    s.close()
-    if not result:
-        return False
-    else:
-        return True
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(10)
+        result = s.connect_ex((ip, port))
+    return True if result else False
 
 
-def isdown(ip, port, retry):
+def check_remote_status(ip, port, retry):
     """ Attempts to connect to an ip with retries if the ip/port are
     not responding
         ip(str) - ip address of target
@@ -62,89 +56,107 @@ def isdown(ip, port, retry):
     retry(int) - number of times to retry
     Returns time.time() if offline, True if online
     """
-    for x in range(1, retry+1):
+    # alert('!', 'DEBUG: {} PROC STARTED'.format(ip))
+    for x in range(1, retry + 1):
         if not portdown(ip, port):
-            return ip, False
-        else:
-            message('x', "Site {} Down. Attempt {} of {} ".format(ip, x, retry))
-    return ip, time.time()
-        
+            break
+    else:
+        # alert('!', 'DEBUG: {} PROC FINISHED'.format(ip))
+        return ip, time.time()
+    # alert('!', 'DEBUG: {} PROC FINISHED'.format(ip))
+    return ip, False
 
-def callforhelp(site, ip, port, dtime, creds):
+
+def call_for_help(site, ip, port, down, creds):
     """Creates and sends an email
     site(str) - site name
     ip(str) - site ip
     port(int) - site port
-    dtime(str) - time and date
+    down(str) - time and date
     creds(dict) - contains to, from, subject fields
     """
-    msg = """WARNING! {name} IS OFFLINE!
-    Last online: {dtime}
-    Cannot connect to {ip} via port {port}
-
-    Please contact the {name} site and possibly the ISP
-
-    I AM A BOT - DO NOT REPLY
-    """.format(name=site, dtime=dtime, ip=ip, port=port)
+    body = (
+        "WARNING! {site} IS OFFLINE!\nLast online {down}"
+        "Cannot connect to {ip}:{port}"
+        "Please contact {site} to verify if there is a known issue.\n"
+        "TIP: Power cycling the Shaw cable modem often fixes intermittent issues. "
+        "This involves unplugging the modem from the power for 30 seconds, "
+        "then plugging it back in, but be careful not to power cycle the wrong device.\n"
+        "If the issue persists for more than 15 minutes and the store infrastructure seems fine, "
+        "contact Shaw Technical Support @ 1-877-742-9249\n\n"
+        "This alert was auto-generated. Do not reply").format(
+            site=site, down=down, ip=ip, port=port)
     return emailer.compose_and_send(
-        creds['from'], creds['to'], creds['subject'], msg
-        )
+        creds['from'], creds['to'], creds['subject'], body
+    )
 
 
-def engine(sites, port, creds, retry):
-    while True:
-        if portdown('8.8.8.8', 53) == False:
-            with multiprocessing.Pool(processes=3) as pool:
-                result = pool.starmap(
-                    isdown, {(ip, port, retry) for ip in sites.keys()}, 
-                    )
-            for tup in result:
-                ip = tup[0]
-                dtime = tup[1]
-                if not dtime:
-                    sites[ip]['dtime'] == False
-                else:
-                    if sites[ip]['dtime'] == False:
-                        sites[ip]['dtime'] = dtime
-            for ip, val in sites.items():
-                if val['dtime'] and time.time() - val['emailed'] > 14400:
-                    callforhelp(val['name'], ip, port, time.ctime(val['dtime']), creds)
-                    val['emailed'] = time.time()
-                    message('!', "Email sent! :: {site}".format(site=val['name']))
-                elif val['dtime'] > 0 and time.time() - val['emailed'] < 14400:
-                    message(
-                        '-', 'Skipped {site} - It was emailed {time} min ago'.format(
-                            site=val['name'], time=(time.time() - val['emailed'])//60)
-                            )
-            message(' ', "{time} Sleeping for 15mins".format(time=time.ctime()))
-            time.sleep(900)
+def internet_working(ip='8.8.8.8', port=53):
+    """Checks if socket can connect to a remote ip, Google DNS by default
+    """
+    return not portdown(ip, port)
+
+
+def recently_emailed(emailed):
+    return time.time() - emailed < 14400
+
+
+def updatedict(sites, result):
+    """Updates a dictionary based on the fields in a list of tuples"""
+
+    for ip, down in result:
+        if down and sites[ip]['down'] == False:
+            sites[ip]['down'] = down
         else:
-            message('x', "Can't reach Google. Sleeping for an hour")
-            time.sleep(3600)
-
-def generatedict(sites_ips):
-    # removed dtime check... keep simple
-    sites = {}
-    for site, ip in sites_ips.items():
-        sites[ip] = {
-            'name': site,
-            'dtime': False,
-            'emailed': False,
-        }
+            sites[ip]['down'] = False
     return sites
 
 
-def main():
-    pass
-    
+def try_email(sites, creds):
+
+    for ip, val in sites.items():
+        if val['down']:
+            if not recently_emailed(val['emailed']):
+                call_for_help(
+                    sites[ip]['name'], ip, port, time.ctime(sites[ip]['down']), creds
+                    )
+                sites[ip]['emailed'] = time.time()
+                alert('x', 'email sent')
+            else:
+                alert('-', 'Skipped {site} - It was emailed {time} min ago'.format(
+                    site=sites[ip]['name'], time=(time.time() - sites[ip]['emailed']) // 60))
+
+
+def main(sites, port, creds, retry):
+
+    if internet_working():
+        with multiprocessing.Pool(processes=3) as pool:
+            result = pool.starmap(
+                check_remote_status, ((ip, port, retry) for ip in sites.keys()),
+            )
+
+        sites = updatedict(sites, result)
+
+        try_email(sites, creds)
+
+        alert('-', "Scan complete. Sleeping for 15mins".format(time=time.ctime()))
+        time.sleep(900)
+
+    else:
+        alert('x', "Can't reach Google. Sleeping for an hour")
+        time.sleep(3600)
+
 
 if __name__ == '__main__':
+    retry = 5
     settings = loadconf('sitecheck.config.json')
     port = settings['CONF']['port']
     creds = settings['EMAIL']
-    retry = 5
-    sites = generatedict(settings['DEST'])
+    sites = {
+        ip: { 
+            'name': site, 'down': False, 'emailed': False
+            } for site, ip in settings['DEST'].items()
+    }
 
-    engine(sites, port, creds, retry)
-
-    
+    while True:
+        main(sites, port, creds, retry)
