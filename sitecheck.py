f2ce0dd2f7254ff83ba66d8d57e94f7197d2966d
MyBagofTricks <ipulldarters@gmail.com>
2017-06-26 16:12:53 -0700

    cleaned up code and removed superfluous logging

--- sitecheck.py
+++ sitecheck.py
@@ -5,13 +5,24 @@ import socket
 import time
 import json
 import os
+import logging
 import multiprocessing
 
 import emailer
 
+logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.CRITICAL)
+logging.getLogger('googleapiclient.discovery').setLevel(logging.CRITICAL)
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)-4s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
 
 def loadconf(f_name):
-    """ Checks for a json file, returns data if it exists, or else exits
+    """ Checks for a json file, Return config settings(dict) data if file exists, or else exits
+
     f_name(str) - name of the file
     """
     if os.path.isfile(f_name):
@@ -20,27 +31,22 @@ def loadconf(f_name):
                 data = json.load(json_file)
                 return data
             except ValueError as err:
-                alert('x', "Cannot continue: {err}".format(err=err))
+                logger.info("Cannot continue: {err}".format(err=err))
                 raise SystemExit
     else:
-        alert('x', "{f_name} not found. Check the readme. Exiting".format(f_name=f_name))
+        logger.info(
+            "{f_name} not found. Check the readme. Exiting".format(f_name=f_name))
         raise SystemExit
-
-
-def alert(status, text):
-    """ Formats alerts and prints to console
-    status(str) - error type (x means bad)
-    text(str) - text to print
-    """
-    print("[{status}] {time} :: {text}".format(
-        status=status, time=time.ctime(), text=text))
 
 
 def portdown(ip, port):
     """ Checks if site is down
+
     ip(str) - ip address
+
     port(int) - port number
-    Returns true if down, false if up
+
+    Return True if down, False if up
     """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.settimeout(10)
@@ -49,46 +55,26 @@ def portdown(ip, port):
 
 
 def check_remote_status(ip, port, retry):
-    """ Attempts to connect to an ip with retries if the ip/port are
+    """ Attempts to connect to an ip with 10 retries if the ip/port are
     not responding
-        ip(str) - ip address of target
+
+    ip(str) - ip address of target
+
     port(int) - port address to use
+
     retry(int) - number of times to retry
+
     Returns time.time() if offline, True if online
     """
-    # alert('!', 'DEBUG: {} PROC STARTED'.format(ip))
+    logger.debug("Site check started")
     for x in range(1, retry + 1):
         if not portdown(ip, port):
             break
     else:
-        # alert('!', 'DEBUG: {} PROC FINISHED'.format(ip))
+        logger.debug("Site check complete")
         return ip, time.time()
-    # alert('!', 'DEBUG: {} PROC FINISHED'.format(ip))
+    logger.debug("Site check complete")
     return ip, False
-
-
-def call_for_help(site, ip, port, down, creds):
-    """Creates and sends an email
-    site(str) - site name
-    ip(str) - site ip
-    port(int) - site port
-    down(str) - time and date
-    creds(dict) - contains to, from, subject fields
-    """
-    body = (
-        "WARNING! {site} IS OFFLINE!\nLast online {down}"
-        "Cannot connect to {ip}:{port}"
-        "Please contact {site} to verify if there is a known issue.\n"
-        "TIP: Power cycling the Shaw cable modem often fixes intermittent issues. "
-        "This involves unplugging the modem from the power for 30 seconds, "
-        "then plugging it back in, but be careful not to power cycle the wrong device.\n"
-        "If the issue persists for more than 15 minutes and the store infrastructure seems fine, "
-        "contact Shaw Technical Support @ 1-877-742-9249\n\n"
-        "This alert was auto-generated. Do not reply").format(
-            site=site, down=down, ip=ip, port=port)
-    return emailer.compose_and_send(
-        creds['from'], creds['to'], creds['subject'], body
-    )
 
 
 def internet_working(ip='8.8.8.8', port=53):
@@ -97,35 +83,87 @@ def internet_working(ip='8.8.8.8', port=53):
     return not portdown(ip, port)
 
 
-def recently_emailed(emailed):
-    return time.time() - emailed < 14400
-
-
-def updatedict(sites, result):
-    """Updates a dictionary based on the fields in a list of tuples"""
-
+def update_sites(sites, result):
+    """Updates a dict with values from a tuple
+    
+    sites(dict) - tracks which stores are up/down
+    
+    result(tup) - in the format of ip, down). Down is False if site up
+    
+    Return the updated sites(dict) """
+    sites_down = []
     for ip, down in result:
-        if down and sites[ip]['down'] == False:
-            sites[ip]['down'] = down
+        if down:
+            sites_down.append(sites[ip]['name'])
+            if sites[ip]['down'] == False:
+                sites[ip]['down'] = down
         else:
             sites[ip]['down'] = False
+    if sites_down:
+        logger.warning("{down} currently offline.".format(
+            down=", ".join(sites_down)))
+
     return sites
 
 
-def send_email(sites, creds):
-    
+def total_down(sites):
+    """Returns a string of sites which are not online, based on sites(dict)"""
+    down = ", ".join(val['name'] for val in sites.values() if val['down'])
+    if down:
+        return down
+    else:
+        return None
 
-    for ip, val in sites.items():
-        if val['down']:
-            if not recently_emailed(val['emailed']):
-                call_for_help(
-                    sites[ip]['name'], ip, port, time.ctime(sites[ip]['down']), creds
-                    )
-                sites[ip]['emailed'] = time.time()
-                alert('x', 'email sent')
-            else:
-                alert('-', 'Skipped {site} - It was emailed {time} min ago'.format(
-                    site=sites[ip]['name'], time=(time.time() - sites[ip]['emailed']) // 60))
+
+def build_body(name, down, ip, port):
+    """Builds a generic email template
+
+    name(str) - site's name
+
+    down(long) - epoc time of last time the site was online
+
+    ip(str) - ip address of site
+
+    port(int) - port scanned
+
+    Returns the email body text"""
+    return (
+        "WARNING! {name} IS OFFLINE!\nLast online {down}"
+        "Cannot connect to {ip}:{port}"
+        "Please contact {name} to verify if there is a known issue.\n"
+        "TIP: Power cycling the Shaw cable modem often fixes intermittent issues. "
+        "This involves unplugging the modem from the power for 30 seconds, "
+        "then plugging it back in, but be careful not to power cycle the wrong device.\n"
+        "If the issue persists for more than 15 minutes and the store infrastructure seems fine, "
+        "contact Shaw Technical Support @ 1-877-742-9249\n\n"
+        "This alert was auto-generated. Do not reply").format(
+            name=name, down=time.ctime(down), ip=ip, port=port)
+
+
+def recently_emailed(emailed):
+    """Return True if emailed(float) is less than 4hours from time.time, else False"""
+    return time.time() - emailed < 14400
+
+
+def send_email(name, ip, port, down, creds):
+    """Assembles and sends an email
+
+    name(str) - site name
+
+    ip(str) - site ip
+
+    port(int) - site port
+
+    down(str) - last online epoch time
+
+    creds(dict) - contains to, from, subject fields
+
+    Returns the message id if sucessful
+    """
+    body = build_body(name, down, ip, port)
+    return emailer.compose_and_send(
+        creds['from'], creds['to'], creds['subject'], body
+    )
 
 
 def main(sites, port, creds, retry):
@@ -133,19 +171,23 @@ def main(sites, port, creds, retry):
     if internet_working():
         with multiprocessing.Pool(processes=3) as pool:
             result = pool.starmap(
-                check_remote_status, ((ip, port, retry) for ip in sites.keys()),
+                check_remote_status, ((ip, port, retry)
+                                      for ip in sites.keys()),
             )
+        site_status = update_sites(sites, result)
 
-        sites = updatedict(sites, result)
-
-        send_email(sites, creds)
-
-        alert('-', "Scan complete. Sleeping for 15mins".format(time=time.ctime()))
+        for ip, v in site_status.items():
+            if v['down']:
+                if not recently_emailed(v['emailed']):
+                    send_email(v['name'], ip, port, v['down'], creds)
+                    site_status[ip]['emailed'] = time.time()
+                    logger.info(
+                        '{name} down. Email sent'.format(name=v['name']))
         time.sleep(900)
-
     else:
-        alert('x', "Can't reach Google. Sleeping for an hour")
-        time.sleep(3600)
+        logger.error(
+            "Can't reach Google - Is the internet down? Retrying in 30mins.")
+        time.sleep(1800)
 
 
 if __name__ == '__main__':
@@ -154,10 +196,9 @@ if __name__ == '__main__':
     port = settings['CONF']['port']
     creds = settings['EMAIL']
     sites = {
-        ip: { 
+        ip: {
             'name': site, 'down': False, 'emailed': False
-            } for site, ip in settings['DEST'].items()
+        } for site, ip in settings['DEST'].items()
     }
-
     while True:
         main(sites, port, creds, retry)
