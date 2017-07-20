768a5eba571c7c3a86e5cd9bfa05235149667634
MyBagofTricks <ipulldarters@gmail.com>
2017-07-20 15:36:05 -0700

    quiet hours fixed

--- sitecheck.py
+++ sitecheck.py
@@ -53,14 +53,14 @@ def check_remote_status(ip, port, retry):
 
     Returns time.time() if offline, True if online
     """
-    logger.debug("Started Site {}".format(ip))
+    logger.debug(f"Started Site {ip}")
     for x in range(1, retry + 1):
         if not portdown(ip, port):
             break
     else:
-        logger.debug("Complete Site {}".format(ip))
+        logger.debug(f"Complete Site {ip}")
         return ip, time.time()
-    logger.debug("Complete Site {}".format(ip))
+    logger.debug(f"Complete Site {ip}")
     return ip, False
 
 
@@ -69,24 +69,28 @@ def internet_working(ip='8.8.8.8', port=53):
     return not portdown(ip, port)
 
 
-def build_body(name, down, ip, port):
-    """Return email body text (str) based on parameters name(str), down(long),, ip(str), port(int)"""
-    return (
-        "WARNING! {name} IS OFFLINE!\nLast online {down}"
-        "Cannot connect to {ip}:{port}\n\n"
-        "Please contact {name} to verify if there is a known issue.\n"
-        "TIP: Power cycling the Shaw cable modem often fixes intermittent issues. "
-        "This involves unplugging the modem from the power for 30 seconds, "
-        "then plugging it back in, but be careful not to power cycle the wrong device.\n"
-        "If the issue persists for more than 15 minutes and the store infrastructure seems fine, "
-        "contact Shaw Technical Support @ 1-877-742-9249\n\n"
-        "This alert was auto-generated. Do not reply").format(
-            name=name, down=time.ctime(down), ip=ip, port=port)
-
-
 def recently_emailed(emailed):
     """Return True if emailed(float epoc time) is less than 4hours ago, else False"""
     return time.time() - emailed < 14400
+
+
+def quiet_hours(start, stop, time_to_test=int(time.strftime("%H"))):
+    """Return True if time_to_test(int) is between start(int), stop(int), time_to_test(int)"""
+    if start > stop:
+        return time_to_test >= start or time_to_test <= stop
+    else:
+        return time_to_test >= start and time_to_test <= stop
+
+
+def build_body(name, down, ip, port, advice):
+    """Return email body text (str) based on parameters name(str), down(long), ip(str), port(int), advice(string)"""
+    return "\n\n".join((
+        f"WARNING! Site: {name.capitalize()} IS OFFLINE!",
+        f"Last online {time.ctime(down)}",
+        f"Details :: could not connect to {ip} on port {port}",
+        "Please contact the site to verify if this is a known issue.",
+        f"TIP: {advice}",
+        "This alert was auto-generated.\nDo not reply"))
 
 
 def send_email(name, ip, port, down, creds):
@@ -104,29 +108,23 @@ def send_email(name, ip, port, down, creds):
 
     Returns the message id if sucessful
     """
-    body = build_body(name, down, ip, port)
+    body = build_body(name, down, ip, port, creds['advice'])
     return emailer.compose_and_send(
         creds['from'], creds['to'], creds['subject'], body
     )
 
 
-def quiet_hours(start, stop, time_to_test=int(time.strftime("%H"))):
-    """Return True if time_to_test(int) is between start(int), stop(int), time_to_test(int)"""
-    return time_to_test >= start or time_to_test <= stop
-
-
 def engine(sites, config, creds):
-    
     port = int(config.get('port'))
     retry = int(config.get('retry'))
     quiet_start = int(config.get('quiet_start'))
     quiet_stop = int(config.get('quiet_stop'))
-
     if internet_working():
         logger.info('Scan started')
         with multiprocessing.Pool(processes=3) as pool:
             result = pool.starmap(
-                check_remote_status, ((ip, port, retry) for ip in sites.keys()),
+                check_remote_status, ((ip, port, retry)
+                                      for ip in sites.keys()),
             )
         for ip, down in result:
             if down:
@@ -134,15 +132,17 @@ def engine(sites, config, creds):
                     sites[ip]['down'] = down
                 if not quiet_hours(quiet_start, quiet_stop):
                     if not recently_emailed(sites[ip].get('emailed', 0)):
-                        send_email(sites[ip]['name'], ip, port, sites[ip]['down'], creds)
+                        send_email(sites[ip]['name'], ip, port,
+                                   sites[ip]['down'], creds)
                         sites[ip]['emailed'] = time.time()
-                        logger.warning('{name} down. Email sent'.format(name=sites[ip]['name']))
+                        logger.warning(f"{sites[ip]['name']} down. Email sent")
                 else:
-                    logger.warning("{} is down, but quiet hours in effect".format(sites[ip]['name']))
+                    logger.warning(
+                        f"{sites[ip]['name']} is down, but quiet hours in effect")
     else:
         logger.error("Google unreachable. Check internet connection.")
     return sites
-        
+
 
 def main(config_file):
     sites, config, email_creds = parse_config(config_file)
