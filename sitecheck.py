a6b2c253416eba8c63a9061453861091b8dc8fa1
MyBagofTricks <ipulldarters@gmail.com>
2017-08-02 21:06:07 -0700

    added unittests

--- sitecheck.py
+++ sitecheck.py
@@ -23,27 +23,10 @@ def parse_config(config_file):
     config = configparser.SafeConfigParser()
     config.read(config_file)
     sites = {ip: {'name': site} for site, ip in config['sites'].items()}
-    settings = config['settings']
-    email_creds = config['email']
-    return sites, settings, email_creds
+    return sites, config['settings'], config['email']
 
 
-def portdown(ip, port):
-    """ Checks if site is down
-
-    ip(str) - ip address
-
-    port(int) - port number
-
-    Return True if down, False if up
-    """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.settimeout(10)
-        result = s.connect_ex((ip, port))
-    return True if result else False
-
-
-def check_site_status(ip, port, retry=10):
+def check_site_status(ip, port, retry=5):
     """ Attempts to connect to an ip with 10 retries if the ip/port are not responding
 
     ip(str) - ip address of target
@@ -52,20 +35,17 @@ def check_site_status(ip, port, retry=10):
 
     retry(int) - (optional) number of times to retry
 
-    Returns time.time() if offline, True if online
+    Return time.time() if offline, True if online
     """
-    logger.debug(f"{ip} Started")
-    for x in range(1, retry + 1):
+    for x in range(1, retry+1):
+        logger.debug(f"{ip} Scanning")
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(10)
             result = s.connect_ex((ip, port))
+        logger.debug(f'{ip} Completed')
         if not result:
-            break
-        else:
-            logger.debug(f'{ip} Complete')
-            return ip, time.time()
-    logger.debug(f'{ip} Complete')
-    return ip, None
+            return ip, None
+    return ip, time.time()
 
 
 def internet_working(ip='8.8.8.8', port=53):
@@ -86,73 +66,70 @@ def quiet_hours(start, stop, time_to_test=int(time.strftime("%H"))):
         return time_to_test >= start and time_to_test <= stop
 
 
-def build_body(name, down, ip, port, advice):
+def build_body(name, ip, port, down, advice):
     """Return email body text (str) based on parameters name(str), down(long), ip(str), port(int), advice(string)"""
-    return "\n\n".join((
+    body = "\n\n".join((
         f"WARNING! Site: {name.capitalize()} IS OFFLINE!",
         f"Last online {time.ctime(down)}",
         f"Details :: could not connect to {ip} on port {port}",
         "Please contact the site to verify if this is a known issue.",
         f"TIP: {advice}",
         "This alert was auto-generated.\nDo not reply"))
+    return body
 
 
-def send_email(name, ip, port, down, creds):
+def send_email(body, creds):
     """Assembles and sends an email
 
-    name(str) - site name
-
-    ip(str) - site ip
-
-    port(int) - site port
-
-    down(str) - last online epoch time
+    body(str) - body text of the e-mail
 
     creds(dict) - contains to, from, subject fields
 
     Returns the message id if sucessful
     """
-    body = build_body(name, down, ip, port, creds['advice'])
     return emailer.compose_and_send(
         creds['from'], creds['to'], creds['subject'], body
     )
 
 
 def engine(sites, config, creds):
+    
     port = int(config.get('port'))
     retry = int(config.get('retry'))
+    check_status = partial(check_site_status, port=port, retry=retry)
     quiet_start = int(config.get('quiet_start'))
     quiet_stop = int(config.get('quiet_stop'))
+
     if internet_working():
         logger.info('Scan started')
         with multiprocessing.Pool(processes=10) as pool:
-            result = pool.starmap(
-                check_site_status, ((ip, port)
-                for ip in sites.keys()),
-                )
-        for ip, down in result:
+            results = pool.map(check_status, sites.keys(),)
+        for ip, down in results:
             if down:
-                if not sites[ip].get('down'):
+                if 'down' not in sites[ip]:
                     sites[ip]['down'] = down
                 if not quiet_hours(quiet_start, quiet_stop):
                     if not recently_emailed(sites[ip].get('emailed', 0)):
-                        send_email(sites[ip]['name'], ip, port,
-                                   sites[ip]['down'], creds)
+                        body = build_body(sites[ip]['name'], ip, port, sites[ip]['down'], creds['advice'])
+                        send_email(body, creds)
                         sites[ip]['emailed'] = time.time()
-                        logger.warning(f"{sites[ip]['name']} down. Email sent")
+                        logger.info(f"{sites[ip]['name']} down. Email sent")
                 else:
-                    logger.warning(
+                    logger.info(
                         f"{sites[ip]['name']} is down, but quiet hours in effect")
+            elif not down and 'down' in sites[ip]:
+                sites[ip].pop('down')
     else:
         logger.error("Google unreachable. Check internet connection.")
     return sites
 
 
 def main(config_file):
+
     sites, config, email_creds = parse_config(config_file)
     while True:
         sites = engine(sites, config, email_creds)
-        time.sleep(900)
+        time.sleep(30)
 
 
 if __name__ == '__main__':
---
+++ tests/test_sitecheck.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import unittest
+import sitecheck
+import time
+
+class TestQuietHours(unittest.TestCase):
+    def test_sitecheck_true_12_4_22(self):
+        result = sitecheck.quiet_hours(12, 4, 22)
+        self.assertEqual(result, True)
+
+    def test_sitecheck_false_22_2_12(self):
+        result = sitecheck.quiet_hours(22, 2, 12)
+        self.assertEqual(result, False)
+
+    def test_sitecheck_false_23_1_5(self):
+        result = sitecheck.quiet_hours(23, 1, 5)
+        self.assertEqual(result, False)
+
+    def test_sitecheck_true_22_5_22(self):
+        result = sitecheck.quiet_hours(22, 5, 22)
+        self.assertEqual(result, True)
+
+    def test_sitecheck_true_22_5_5(self):
+        result = sitecheck.quiet_hours(22, 5, 5)
+        self.assertEqual(result, True)
+
+    def test_sitecheck_true_9_17_12(self):
+        result = sitecheck.quiet_hours(9, 17, 12)
+        self.assertEqual(result, True)
+
+    def test_sitecheck_false_23_5_18(self):
+        result = sitecheck.quiet_hours(23, 5, 18)
+        self.assertEqual(result, False)
+
+    def test_sitecheck_false_23_5_9(self):
+        result = sitecheck.quiet_hours(23, 5, 9)
+        self.assertEqual(result, False)
+
+
+class TestIfPortIsDown(unittest.TestCase):
+    def test_google_53_false(self):
+        result = sitecheck.check_site_status('8.8.8.8', 53, 1)
+        self.assertIsNone(result[1])
+    def test_google_52_false(self):
+        result = sitecheck.check_site_status('8.8.8.8', 52, 1)
+        self.assertIsNotNone(result[1])
+
+
+
+
+class TestRecentlyEmailed(unittest.TestCase):
+    
+    def test_recently_emailed_false(self):
+        result = sitecheck.recently_emailed(time.time()-100)
+        self.assertEqual(result, True)
+
+    def test_emailable_true(self):
+        result = sitecheck.recently_emailed(time.time()-18000)
+        self.assertEqual(result, False)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
